3e91cea4e4d63eccf35e08ccd5145ae04e616579
bongkyunSON <BK.Son815@gmail.com>
2023-02-09 01:31:22 +0900

    refact:

--- sbk/Prophet_Class.py
+++ sbk/Prophet_Class.py
@@ -20,63 +20,52 @@ from plotnine import *
 
 
 
-class Prophet_Class:
-    
-    def __init__(self, df: pd.DataFrame, target: str, param_grid: dict): 
-        
-        
+class Prophet_model:
+
+    def __init__(self, df: pd.DataFrame, param_grid: dict, matrics: str, periods=30, horizon='30 days'): 
         self.df = df
-        
-        self.df['y'] = target
-        
+        self.df['y'] = df['y']
         self.df['ds'] = df['ds']
-    
         self.param_grid = param_grid
-        
-
-    
-    
-    def metrics(self, metrix: str, horizon: str):
-
-    # horizon 사용시 e.g.'365 days'
-    # metrix = mse rmse mae mdape smape coverage 
+        self.matrics = matrics
+        self.periods = periods
+        self.horizon = horizon
 
         all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
-        evaluation_metrix = []  
+        evaluation_metrix = []  # Store the RMSEs for each params here
+        # 하이퍼 파라미터만 조정
+        
+        holiday = pd.DataFrame([])
+        for date, name in sorted(holidays.KR(years=[2018,2019,2020,2021]).items()):
+            # years만 조정
+            holiday = holiday.append(pd.DataFrame({'ds': date, 'holiday' : "KR-Holidays"}, index=[0]), ignore_index=True)
+        holiday['ds'] = pd.to_datetime(holiday['ds'], format='%Y-%m-%d', errors='ignore')
+        
 
         for params in all_params:
-
             model = Prophet(**params)
-            model.fit(df)  
-
-            df_cv = cross_validation(model, horizon=horizon, parallel="processes")
-
+            model.fit(df)  # Fit model with given params
+            df_cv = cross_validation(model, horizon=self.horizon, parallel="processes")
             df_p = performance_metrics(df_cv)
-
-            evaluation_metrix.append(df_p[metrix].values[0])
+            evaluation_metrix.append(df_p[self.matrics].values[0])
 
         tuning_results = pd.DataFrame(all_params)
-        tuning_results[metrix] = evaluation_metrix
+        tuning_results[self.matrics] = evaluation_metrix
 
+        # Sorted by rmes values
+        tuning_results = tuning_results.sort_values(by=[self.matrics])
 
-        tuning_results = tuning_results.sort_values(by=[metrix])
-
-
+        # Pick the optimized(having minimum rmse value) hyper-parameter combination
         self.final_params = tuning_results.iloc[0, :-1].to_dict()
 
+        # Train the final model with optimized params
+        self.m = Prophet(**self.final_params, holidays=holiday).fit(df)
+        future = self.m.make_future_dataframe(periods=self.periods, freq='D')
+        self.forecast = self.m.predict(future)
 
-
-
-        def final(periods: int, freq: str):
-
-            m = Prophet(**final_params).fit(df)
-            future = m.make_future_dataframe(periods=periods, freq=freq)
-            forecast = m.predict(future)
-
-
-            return final_params, forecast.loc[df.shape[0]:, 'yhat'].values
-
-
+    def get_results(self):
+        return self.final_params, self.forecast.loc[self.df.shape[0]:, 'yhat'].values
+        # 마지막 리턴값만 조정
 
         
     
